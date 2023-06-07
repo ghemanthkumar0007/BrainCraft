a351e4ca23e41b060f7d7531bbbe832989f8fe40
ghemanthkumar0007 <135703428+ghemanthkumar0007@users.noreply.github.com>
2023-06-07 11:52:35 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary1:  "#0B1437"  ,//"#4338ca",
-        secondary1: "#111C44",//"#1F2937" ,//"#4f46e5",
-        sidebar1: "#0B1437",//"#1f2937"
-
-        primary:  "#fff"  ,//"#4338ca",
-        secondary: "#e5e7eb",//"#1F2937" ,//"#4f46e5",
-        sidebar: "#fff",//"#1f2937"
-
-        // whgpk: '#000000',
-        // blgpk: '#fff'
-        whgpk: '#fff',
-        blgpk: '#000000'
-
-      }
+        primary: "#0B1437",
+        secondary: "#111C44",
+        sidebar: "#0B1437",
+      },
     },
   },
   plugins: [],
-}
+};
